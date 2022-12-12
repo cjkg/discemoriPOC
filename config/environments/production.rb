2cc712bfd4a63a1161958ad20bb3295f7bdabdc6
cjkg <cjgibilisco@gmail.com>
2022-12-12 08:27:20 -0600

    updated prod configs to avoid deprecated sass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #disables deprecated Sass
+  config.assets.css_compressor = nil
 end
